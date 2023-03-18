167d1255b02e2731f2c4da9d247538985e7e9fa0
Roman lesyo <ladiginscormag@gmail.com>
2023-03-18 13:26:44 +0200

    added news

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,29 @@
+import { getAllNews } from '../API';
+import { useEffect, useState } from 'react';
+
 export const App = () => {
-  return <div>Petly-site</div>;
+  const [transactionList, setTransactionList] = useState([]);
+  useEffect(() => {
+    async function get() {
+      try {
+        const { data } = await getAllNews();
+        setTransactionList(data);
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    get();
+  }, []);
+  return (
+    <>
+      {transactionList.map(({ _id, date, description, url }) => (
+        <ul key={_id}>
+          <li>{date}</li>
+          <li>{description}</li>
+          <li>{url}</li>
+        </ul>
+      ))}
+    </>
+  );
 };
