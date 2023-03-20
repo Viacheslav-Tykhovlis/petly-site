f141534862e5be918a58f3ee067268f8c759f34b
Nick Leso <nickleso.work@gmail.com>
2023-03-20 16:26:27 +0200

    add lists with notices

---
+++ src/components/Notices/NoticesCategoriesList/NoticesListGoodHands.jsx
@@ -0,0 +1,12 @@
+import NoticeCategoryItem from '../NoticeCategoryItem/NoticeCategoryItem';
+
+const NoticesListGoodHands = () => {
+  return (
+    <div>
+      <h2>Notice list Good Hands</h2>
+      <NoticeCategoryItem />
+    </div>
+  );
+};
+
+export default NoticesListGoodHands;
---
+++ src/components/Notices/NoticesCategoriesList/NoticesListSell.jsx
@@ -0,0 +1,12 @@
+import NoticeCategoryItem from '../NoticeCategoryItem/NoticeCategoryItem';
+
+const NoticesListSell = () => {
+  return (
+    <div>
+      <h2>Notice list Sell</h2>
+      <NoticeCategoryItem />
+    </div>
+  );
+};
+
+export default NoticesListSell;
