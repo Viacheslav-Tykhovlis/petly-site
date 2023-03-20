9a54abe54eda57c1ea93b083c089d9f99117c626
Ruslan Babkin <ruslan.babkin01@gmail.com>
2023-03-20 15:56:06 +0200

    done markup

--- src/components/OurFriendsList/OurFriendsItem.jsx
+++ src/components/OurFriendsList/OurFriendsItem.jsx
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 import {
   Friend,
   Name,
@@ -10,8 +11,7 @@ import {
   Subtitle,
   // TimeWorkItem,
   // TimeWorkText,
-  // TimeLink,
-  // ImageLink,
+  TimeButton,
   DataLink,
 } from './OurFriendsItem.styled';
 
@@ -25,50 +25,91 @@ export const OurFriendsItem = ({
   phone,
   email,
 }) => {
+  const [isVisibleWorkDays, setisVisibleWorkDays] = useState(false);
+  const openDay = workDays?.find(day => day.isOpen);
+
+  // const handleClicktoWorkdays = () => {
+  //   setisVisibleWorkDays(true);
+  // };
+  console.log(isVisibleWorkDays);
+
   return (
     <Friend>
-      <NameLink href={url}>
+      <NameLink href={url} target="_blank" rel="noopener noreferrer">
         <Name> {title}</Name>
       </NameLink>
 
       <Box>
-        <ImgFriend src={imageUrl} alt="FotoSponsor" />
+        <ImgFriend src={imageUrl} alt="logo friends" />
 
         <DataList>
           <DataItem>
             <Subtitle>Time:</Subtitle>
-            {/* <TimeLink>
-                {workDays
-                  ? workDays.map(({ to, from, isOpen, index }) => (
-                      <TimeWorkItem key={index}>
-                        <TimeWorkText>
-                          {to} - {from}
-                        </TimeWorkText>
-                      </TimeWorkItem>
-                    ))
-                  : '-----------------------------------'}
-            </TimeLink> */}
+            {workDays ? (
+              <TimeButton
+                onClick={() => setisVisibleWorkDays(!isVisibleWorkDays)}
+              >
+                {openDay.from} - {openDay.to}
+              </TimeButton>
+            ) : (
+              <p>-----------------------------------</p>
+            )}
           </DataItem>
+          {isVisibleWorkDays &&
+            workDays.map(({ to, from, isOpen, index }) => (
+              <ul>
+                <li key={index}>
+                  <p>
+                    {from}- {to}
+                  </p>
+                </li>
+              </ul>
+            ))}
 
           <DataItem>
             <Subtitle>Adress:</Subtitle>
-            <DataLink href={addressUrl}>
-              {address ? address : '-----------------------------------'}
-            </DataLink>
+            {address ? (
+              <DataLink
+                style={{ textDecoration: 'underline' }}
+                href={addressUrl}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {address}
+              </DataLink>
+            ) : (
+              <p>----------------------------------</p>
+            )}
           </DataItem>
 
           <DataItem>
             <Subtitle>Email:</Subtitle>
-            <DataLink href={email}>
-              {email ? email : '-----------------------------------'}
-            </DataLink>
+            {email ? (
+              <DataLink
+                href={`mailto:${email}`}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {email}
+              </DataLink>
+            ) : (
+              <p>----------------------------------</p>
+            )}
           </DataItem>
 
           <DataItem>
             <Subtitle>Phone:</Subtitle>
-            <DataLink href={phone}>
-              {phone ? phone : '-----------------------------------'}
-            </DataLink>
+            {phone ? (
+              <DataLink
+                href={`tel:${phone}`}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {phone}
+              </DataLink>
+            ) : (
+              <p>----------------------------------</p>
+            )}
           </DataItem>
         </DataList>
       </Box>
@@ -92,3 +133,19 @@ OurFriendsItem.propTypes = {
     }),
   ),
 };
+
+//  {
+//    workDays ? (
+//      workDays.map(({ to, from, isOpen, index }) => (
+//        <TimeLink>
+//          <TimeWorkItem key={index}>
+//            <TimeWorkText>
+//              {to} - {from}
+//            </TimeWorkText>
+//          </TimeWorkItem>
+//        </TimeLink>
+//      ))
+//    ) : (
+//      <p>-----------------------------------</p>
+//    );
+//  }
--- src/components/OurFriendsList/OurFriendsItem.styled.jsx
+++ src/components/OurFriendsList/OurFriendsItem.styled.jsx
@@ -18,15 +18,15 @@ export const Friend = styled.li`
   filter: drop-shadow(7px 4px 14px rgba(49, 21, 4, 0.07));
 
   @media screen and (min-width: 768px) {
-    flex-basis: calc((100% - 30px) / 2);
+    flex-basis: calc((100% - 32px) / 2);
     border-radius: 40px;
     padding: 16px 4px;
     font-size: 14px;
     line-height: 1, 36;
   }
 
-  @media screen and (min-width: 1200px) {
-    flex-basis: calc((100% - 60px) / 3);
+  @media screen and (min-width: 1280px) {
+    flex-basis: calc((100% - 64px) / 3);
     font-size: 16px;
     line-height: 1, 375;
   }
@@ -35,20 +35,24 @@ export const Friend = styled.li`
 export const NameLink = styled.a`
   color: ${props => props.theme.colors.accent};
   text-decoration-color: ${props => props.theme.colors.accent};
-  cursor: pointer;
 `;
 
 export const Name = styled.h2`
+  margin-bottom: 12px;
   color: ${props => props.theme.colors.accent};
   font-weight: ${props => props.theme.fontWeight.bold};
   text-align: center;
-  margin-bottom: 12px;
   font-size: 12px;
   line-height: 1, 33;
 
   @media screen and (min-width: 768px) {
+    margin-bottom: 16px;
     font-size: 16px;
     line-height: 1, 375;
+  }
+  @media screen and (min-width: 1280px) {
+    font-size: 20px;
+    line-height: 1, 35;
   }
 `;
 
@@ -57,15 +61,23 @@ export const Box = styled.div`
 `;
 
 export const ImgFriend = styled.img`
-  width: 110px;
-  height: 78px;
+  width: 120px;
+  height: 85px;
+
+  @media screen and (min-width: 768px) {
+    width: 110px;
+    height: 78px;
+  }
+  @media screen and (min-width: 1280px) {
+    width: 158px;
+    height: 112px;
+  }
 `;
 
 export const DataList = styled.ul`
-  margin-left: 12px;
-  max-width: 150px;
   display: flex;
   flex-direction: column;
+  margin-left: 12px;
   gap: 4px;
 
   @media screen and (min-width: 768px) {
@@ -73,21 +85,52 @@ export const DataList = styled.ul`
     gap: 8px;
   }
 
-  @media screen and (min-width: 1200px) {
-    margin-left: 14px;
+  @media screen and (min-width: 1280px) {
+    margin-left: 16px;
+    gap: 12px;
   }
 `;
 
 export const DataItem = styled.li``;
-
 export const Subtitle = styled.p``;
+
+export const DataLink = styled.a`
+  text-decoration: none;
+  color: ${props => props.theme.colors.black};
+  :hover,
+  :focus {
+    color: ${props => props.theme.colors.accent};
+    transition: ${props => props.theme.animation.cubicBezier};
+  }
+`;
 
 export const TimeWork = styled.ul``;
 export const TimeWorkItem = styled.li``;
 export const TimeWorkText = styled.p``;
 
-export const ImageLink = styled.a``;
-
-export const DataLink = styled.a`
+export const TimeButton = styled.button`
+  border: none;
+  background-color: ${props => props.theme.colors.white};
   color: ${props => props.theme.colors.black};
+  font-family: 'Manrope';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 12px;
+  line-height: 1, 33;
+
+  :hover,
+  :focus {
+    color: ${props => props.theme.colors.accent};
+    transition: ${props => props.theme.animation.cubicBezier};
+  }
+
+  @media screen and (min-width: 768px) {
+    font-size: 14px;
+    line-height: 1, 36;
+  }
+
+  @media screen and (min-width: 1280px) {
+    font-size: 16px;
+    line-height: 1, 375;
+  }
 `;
