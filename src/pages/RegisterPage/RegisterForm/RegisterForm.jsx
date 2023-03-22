a6a7d63d4e58c3cb2bda31f4257bcd335cfd09eb
Victor Selischev <95778687+VictorSelischev@users.noreply.github.com>
2023-03-22 17:19:20 +0200

    Update RegisterFormFirst for mobile

--- src/pages/RegisterPage/RegisterForm/RegisterForm.jsx
+++ src/pages/RegisterPage/RegisterForm/RegisterForm.jsx
@@ -1,13 +1,25 @@
-import { Form, InputForm, TitleForm, Button, Paragraph, Link } from './RegisterForm.styled';
+import {
+  Form,
+  InputForm,
+  TitleForm,
+  Button,
+  Paragraph,
+  Link,
+} from './RegisterForm.styled';
 
 const RegisterFormFirst = () => {
   return (
     <>
       <Form action="">
         <TitleForm>Registration</TitleForm>
-        <InputForm type="text" />
-        <InputForm type="text" />
-        <InputForm className="lastinput" type="text" />
+        <InputForm name="email" type="email" placeholder="Email" />
+        <InputForm name="password" type="password" placeholder="Password" />
+        <InputForm
+          name="comfirmPassword"
+          type="password"
+          placeholder="Confirm Password"
+          className="lastinput"
+        />
         <Button type="button">Next</Button>
         <Paragraph>
           Already have an account?&nbsp;<Link to="/login">Login</Link>
