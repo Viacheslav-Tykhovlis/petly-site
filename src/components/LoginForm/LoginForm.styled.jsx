14e19613733cb2fe42555b2663d913b1ae851325
DanStelmakh <danylo.stelmakh@gmail.com>
2023-03-23 11:55:37 +0200

    пофіксив редакс, додав валідацію, виправив стилі

--- src/components/LoginForm/LoginForm.styled.jsx
+++ src/components/LoginForm/LoginForm.styled.jsx
@@ -1,11 +1,24 @@
 import styled from 'styled-components';
+import { Field } from 'formik';
 
 export const LogInWrapper = styled.div`
   display: block;
-  /* outline: 1px solid red; */
-  padding: 0 20px;
   max-width: 100%;
   margin: auto;
+
+  @media screen and (min-width: 768px) {
+    max-width: 608px;
+    background-color: rgba(255, 255, 255, 1);
+    box-shadow: 7px 4px 14px rgba(0, 0, 0, 0.11);
+    border-radius: 40px;
+    padding: 60px 80px;
+    margin-top: 204px;
+  }
+
+  @media screen and (min-width: 1280px) {
+    margin-top: 80px;
+    max-width: 618px;
+  }
 `;
 
 export const LogInTitle = styled.h1`
@@ -13,14 +26,18 @@ export const LogInTitle = styled.h1`
   font-weight: 700;
   text-align: center;
   margin-bottom: 40px;
+  margin-top: 42px;
   letter-spacing: 0.04em;
+  @media screen and (min-width: 768px) {
+    margin-top: 0;
+  }
 `;
 
-export const LogInInput = styled.input`
+export const LogInInput = styled(Field)`
   width: 100%;
   padding: 11px 14px;
-  background: #fdf7f2;
-  border: 1px solid rgba(245, 146, 86, 0.5);
+  background-color: #fdf7f2;
+  border: ${props => props.border || `1px solid rgba(245, 146, 86, 0.5)`};
   border-radius: 40px;
   margin-bottom: 24px;
 `;
@@ -29,14 +46,61 @@ export const LogInButton = styled.button`
   width: 100%;
   background: #f59256;
   border-radius: 40px;
-  border: transparent;
+  border: 2px solid transparent;
   color: rgba(255, 255, 255, 1);
   font-size: 20px;
+  font-weight: 500;
   padding: 8px 0;
   margin-top: 16px;
   margin-bottom: 40px;
+  transition: background-color 0.25s cubic-bezier(0.4, 0, 0.2, 1);
+  &:hover,
+  &:focus {
+    background-color: rgba(255, 255, 255, 1);
+    border: 2px solid #f59256;
+    color: rgba(0, 0, 0, 1);
+  }
 `;
 
-export const LogInText = styled.span`
+export const LogInText = styled.p`
   text-align: center;
+  color: rgba(17, 17, 17, 0.6);
+  font-size: 12px;
+  font-weight: 400;
+  letter-spacing: 0.04em;
+`;
+
+export const Anchor = styled.a`
+  color: rgba(48, 145, 235, 1);
+  font-size: 12px;
+  font-weight: 400;
+  letter-spacing: 0.04em;
+  margin-left: 3px;
+`;
+
+export const ErrorMessage = styled.div`
+  color: rgba(226, 0, 26, 1);
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 1.35;
+  margin-bottom: 24px;
+`;
+
+export const ApproveMessage = styled.div`
+  color: rgba(60, 188, 129, 1);
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 1.35;
+  margin-bottom: 24px;
+`;
+
+export const InputWrapper = styled.div`
+  position: relative;
+`;
+
+export const Icon = styled.svg`
+  position: absolute;
+  content: '';
+  top: 14px;
+  right: 20px;
 `;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
 import storage from 'redux-persist/lib/storage';
-import { persistReducer, persistStore } from 'redux-persist';
 import { logInReducer } from './login/logIn-slice';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 
 const logInPersistConfig = {
   key: 'login',
@@ -13,6 +22,14 @@ export const store = configureStore({
   reducer: {
     logIn: persistReducer(logInPersistConfig, logInReducer),
   },
+  middleware(getDefaultMiddleware) {
+    return getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    });
+  },
+  devTools: process.env.NODE_ENV === 'development',
 });
 
 export const persistor = persistStore(store);
