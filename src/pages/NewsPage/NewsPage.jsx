75993887a2e02785c5d23bdaa91b5a7df036201c
KizimJule <kizimjule@gmail.com>
2023-03-27 11:05:52 +0200

    fix svg

--- src/pages/NewsPage/NewsPage.jsx
+++ src/pages/NewsPage/NewsPage.jsx
@@ -16,8 +16,7 @@ import {
   ErrorText,
 } from './NewsPage.styled';
 import Title from '../../components/Title/Title';
-import { AiOutlineSearch } from 'react-icons/ai';
-import { TiDeleteOutline } from 'react-icons/ti';
+
 import { useSearchParams } from 'react-router-dom';
 
 import { selectNews, selectIsLoading } from 'redux/news/newsSelectors';
@@ -81,11 +80,21 @@ const NewsPage = () => {
 
           {!newsTitle ? (
             <SearchBtn type="submit">
-              <AiOutlineSearch size={23} />
+              <svg width="18" height="18" viewBox="0 0 32 32">
+                <path d="M22.222 19.556h-1.404l-0.498-0.48c1.742-2.027 2.791-4.658 2.791-7.52 0-6.382-5.173-11.556-11.556-11.556s-11.556 5.173-11.556 11.556c0 6.382 5.173 11.556 11.556 11.556 2.862 0 5.493-1.049 7.52-2.791l0.48 0.498v1.404l8.889 8.871 2.649-2.649-8.871-8.889zM11.556 19.556c-4.427 0-8-3.573-8-8s3.573-8 8-8c4.427 0 8 3.573 8 8s-3.573 8-8 8z"></path>
+              </svg>
             </SearchBtn>
           ) : (
             <DeleteBtn type="button" onClick={onClearField}>
-              <TiDeleteOutline size={23} />
+              <svg width="24" height="24" viewBox="0 0 32 32">
+                <path
+                  fill="none"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2.667"
+                  d="M16 28c6.627 0 12-5.373 12-12S22.627 4 16 4 4 9.373 4 16s5.373 12 12 12zM20 12l-8 8M12 12l8 8"
+                />
+              </svg>
             </DeleteBtn>
           )}
         </FilterLabel>
