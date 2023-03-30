5dd8b047521068a1358f65e8a7be94172d51c238
Ruslan Babkin <ruslan.babkin01@gmail.com>
2023-03-30 12:10:55 +0300

    del helpers

--- src/helpers/date.js
+++
@@ -1,9 +0,0 @@
-// convert string to date
-
-export function convertStringToDate(data) {
-  if (data) {
-    const [day, month, year] = data.split('.');
-    const dateObject = new Date(year, month - 1, day);
-    return dateObject;
-  }
-}
